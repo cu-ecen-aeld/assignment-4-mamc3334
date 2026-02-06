66374e5e0821fcc4a595acfcc295fe4c82d4e48e
Mason McGaffin <mamc3334@colorado.edu>
2026-02-05 22:31:33 -0700

    root pwd and dropbear

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c7ac0c5f7e852ee51f93f77c62a48cf8fa6750d6
+AESD_ASSIGNMENTS_VERSION = 755bceacfe0e8f62e04367497bf418526a51d341
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
